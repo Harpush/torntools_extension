e341c4c5668d8cc962798db53dded360be0bb465
Kobe De Peuter <development@kobedepeuter.be>
2026-04-13 12:27:34 +0200

    fix: lock timed updates to avoid trigger it too quickly after each other

--- extension/entrypoints/background/updates.ts
+++ extension/entrypoints/background/updates.ts
@@ -87,81 +87,93 @@ import { getHospitalizationReason, getNextChainBonus, hasFinishedEducation, LINK
 import { getUTCTodayAtTime, hasTimePassed, isSameUTCDay, TO_MILLIS } from "@/utils/common/functions/utilities";
 import { newNotification, notifyUser, storeNotification } from "./notifications";
 
-export function timedUpdates() {
+let lockTimedUpdates = false;
+
+export async function timedUpdates() {
+	if (lockTimedUpdates) return;
+
+	lockTimedUpdates = true;
+
 	const updatePromises: Promise<unknown>[] = [];
-	if (api.torn.key) {
-		updatePromises.push(
-			updateUserdata()
-				.then(({ updated, types, selections }) => {
-					if (updated) console.log(`Updated ${types.join("+")} userdata.`, selections);
-					else console.log("Skipped this userdata update.");
-				})
-				.catch((error) => logError("updating userdata", error)),
-		);
+	try {
+		await loadDatabase();
 
-		updatePromises.push(
-			updateStakeouts()
-				.then(({ updated, success, failed }) => {
-					if (updated) {
-						if (success || failed) console.log("Updated stakeouts.", { success, failed });
-						else console.log("No stakeouts to update.");
-					} else console.log("Skipped this stakeout update.");
-				})
-				.catch((error) => logError("updating stakeouts", error)),
-		);
-
-		updatePromises.push(
-			updateFactionStakeouts()
-				.then(({ updated, success, failed }) => {
-					if (updated) {
-						if (success || failed) console.log("Updated faction stakeouts.", { success, failed });
-						else console.log("No faction stakeouts to update.");
-					} else console.log("Skipped this faction stakeout update.");
-				})
-				.catch((error) => logError("updating faction stakeouts", error)),
-		);
-
-		if (
-			!torndata ||
-			!isSameUTCDay(new Date(torndata.date), new Date()) ||
-			(hasOutdatedTornStats() && hasTimePassed(torndata.date, TO_MILLIS.MINUTES * 10))
-		) {
-			// Update once every torn day.
+		if (api.torn.key) {
 			updatePromises.push(
-				updateTorndata()
-					.then(() => console.log("Updated torndata."))
-					.catch((error) => logError("updating torndata", error)),
+				updateUserdata()
+					.then(({ updated, types, selections }) => {
+						if (updated) console.log(`Updated ${types.join("+")} userdata.`, selections);
+						else console.log("Skipped this userdata update.");
+					})
+					.catch((error) => logError("updating userdata", error)),
 			);
+
+			updatePromises.push(
+				updateStakeouts()
+					.then(({ updated, success, failed }) => {
+						if (updated) {
+							if (success || failed) console.log("Updated stakeouts.", { success, failed });
+							else console.log("No stakeouts to update.");
+						} else console.log("Skipped this stakeout update.");
+					})
+					.catch((error) => logError("updating stakeouts", error)),
+			);
+
+			updatePromises.push(
+				updateFactionStakeouts()
+					.then(({ updated, success, failed }) => {
+						if (updated) {
+							if (success || failed) console.log("Updated faction stakeouts.", { success, failed });
+							else console.log("No faction stakeouts to update.");
+						} else console.log("Skipped this faction stakeout update.");
+					})
+					.catch((error) => logError("updating faction stakeouts", error)),
+			);
+
+			if (
+				!torndata ||
+				!isSameUTCDay(new Date(torndata.date), new Date()) ||
+				(hasOutdatedTornStats() && hasTimePassed(torndata.date, TO_MILLIS.MINUTES * 10))
+			) {
+				// Update once every torn day.
+				updatePromises.push(
+					updateTorndata()
+						.then(() => console.log("Updated torndata."))
+						.catch((error) => logError("updating torndata", error)),
+				);
+			}
+
+			if (!stockdata?.date || hasTimePassed(stockdata.date, TO_MILLIS.MINUTES * 5)) {
+				updatePromises.push(
+					updateStocks()
+						.then(() => console.log("Updated stocks."))
+						.catch((error) => logError("updating stocks", error)),
+				);
+			}
+
+			if (!factiondata || !("date" in factiondata) || hasTimePassed(factiondata.date, TO_MILLIS.MINUTES * 15))
+				updatePromises.push(
+					updateFactiondata()
+						.then(() => console.log("Updated factiondata."))
+						.catch((error) => logError("updating factiondata", error)),
+				);
 		}
 
-		if (!stockdata?.date || hasTimePassed(stockdata.date, TO_MILLIS.MINUTES * 5)) {
-			updatePromises.push(
-				updateStocks()
-					.then(() => console.log("Updated stocks."))
-					.catch((error) => logError("updating stocks", error)),
-			);
-		}
+		updatePromises.push(
+			updateNPCs()
+				.then(({ updated, alerts }) => {
+					if (updated) console.log("Updated npcs.");
+					if (alerts) console.log(`Sent out ${alerts} npc alerts.`);
+				})
+				.catch((error) => logError("updating npcs", error)),
+		);
 
-		if (!factiondata || !("date" in factiondata) || hasTimePassed(factiondata.date, TO_MILLIS.MINUTES * 15))
-			updatePromises.push(
-				updateFactiondata()
-					.then(() => console.log("Updated factiondata."))
-					.catch((error) => logError("updating factiondata", error)),
-			);
+		updatePromises.push(verifyTime().catch((error) => logError("Failed to verify your time to be synced.", error)));
+
+		await Promise.all(updatePromises);
+	} finally {
+		lockTimedUpdates = false;
 	}
-
-	updatePromises.push(
-		updateNPCs()
-			.then(({ updated, alerts }) => {
-				if (updated) console.log("Updated npcs.");
-				if (alerts) console.log(`Sent out ${alerts} npc alerts.`);
-			})
-			.catch((error) => logError("updating npcs", error)),
-	);
-
-	updatePromises.push(verifyTime().catch((error) => logError("Failed to verify your time to be synced.", error)));
-
-	return updatePromises;
 
 	function logError(message: any, error: any) {
 		if (error.code === CUSTOM_API_ERROR.NO_PERMISSION) {
